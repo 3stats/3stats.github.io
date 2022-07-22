99d98d0c5dc27b99e6683135ef1d99b67dc4bef4
espoal <alberto.websci@gmail.com>
2022-07-22 19:11:50 +0200

    branch

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,7 +8,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 const config = {
   title: '[3stats] documentation',
   tagline: 'Dinosaurs are cool',
-  url: 'https://3stats-docs.github.io',
+  url: 'https://3stats.github.io',
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
@@ -17,7 +17,9 @@ const config = {
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: '3stats', // Usually your GitHub org/user name.
-  projectName: '3stats-docs', // Usually your repo name.
+  projectName: '3stats.github.io', // Usually your repo name.
+  trailingSlash: false,
+  deploymentBranch: 'gh-pages',
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
